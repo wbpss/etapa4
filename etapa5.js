fb75ffd5a17a5ce812cbf92612400f9825ab58b7
wbpss <36868011+wbpss@users.noreply.github.com>
2018-06-09 23:37:43 -0300

    Update etapa5.js

--- etapa5.js
+++ etapa5.js
@@ -1,15 +1,18 @@
-var vidas = 3; 
-var pontos = 0;  
+
+var vidas = 10; 
+var pontos = 0; 
+var nivel = 3
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(490,510);
 
 }
 
 function draw() {
-  background(0); 
-  textSize(20);
-  fill(135,206,235);
-  text("Vidas: "+vidas, 20, 60);
-  text("Pontos: "+pontos, 20, 90);
+  background(200); 
+  textSize(15);
+  fill(5,0,0);
+  text("Vidas: "+vidas, 5, 60);
+  text("Pontos: "+pontos, 5, 80);
+  text("Nivel: "+nivel, 5, 100);
 }
